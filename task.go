ca4c678bf2960f5c2223346277b8edbf9cc8766d
suiqingwei <suiqingwei@cmcm.com>
2019-03-18 11:26:52 +0800

    扩展任务bug

--- task.go
+++ task.go
@@ -22,9 +22,10 @@ type taskqueue struct {
 
 //任务通用结构体
 type Task struct {
-	Tid        string   `json:"tid"`       //tid 任务唯一标志符、不能重复 //不取id避免和业务冲突
-	Bid        string   `json:"bid"`       //可能是业务id
-	ExTendTids []string `json:"extendtis"` //一个任务可能有附带的任务、关联的tid、可以一起删除等
+	Tid          string   `json:"tid"`            //tid 任务唯一标志符、不能重复 //不取id避免和业务冲突
+	Bid          string   `json:"bid"`            //可能是业务id
+	ExTendTids   []string `json:"extendtis"`      //一个任务可能有附带的任务、关联的tid、可以一起删除等
+	isExtendTask bool     `json:"is_extend_task"` //是否是扩展任务、如果是扩展任务、则不影响时间
 
 	Extend interface{} `json:"extend"` // 扩展字段 任务具体任务可能需要
 
@@ -141,7 +142,9 @@ func do(t *TimeTask) {
 	if t.Func != nil {
 		t.Func(t.Task)
 		//执行完、则自动加周期
-		UpdateTimeClock(t.Task)
+		if !t.isExtendTask {
+			UpdateTimeClock(t.Task)
+		}
 		return
 	}
 	runDo(t.Task)
